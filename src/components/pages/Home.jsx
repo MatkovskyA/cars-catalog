525759c375d0f22194db429bcd5479268b35d739
MatkovskyA <a.matkovsky14@gmail.com>
2024-05-17 11:10:08 +0300

    Added cars images

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,16 @@
 import styles from "./Home.module.css"
+// import m3 from "assets/img/cars/m3.jpeg"
 
 function Home() {
   return (
-    <div className={styles.item}>
+    <div>
       <h1>Cars catalog</h1>
-      <p>100 000$</p>
-      <button>Read more</button>
+      <div className={styles.item}>
+        <h2>Car 1</h2>
+        {/* <img src={m3} alt="Car" /> */}
+        <p>100 000$</p>
+        <button>Read more</button>
+      </div>
     </div>
   )
 }
